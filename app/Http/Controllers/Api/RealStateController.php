30dcc9416371fa79dc2622c12ab9f39b4e519406
Lucas <lucas.bf@outlook.com>
2023-08-28 19:56:37 -0300

    Criação RealStateController

---
+++ app/Http/Controllers/Api/RealStateController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\RealState;
+use Illuminate\Http\Request;
+
+class RealStateController extends Controller
+{
+
+    /**
+     * @var RealState
+     * @type RealState
+     */
+    private RealState $realState;
+
+    /**
+     * @param RealState $realState
+     */
+    public function __construct(RealState $realState)
+    {
+        $this->realState = $realState;
+    }
+
+    public function index()
+    {
+        $realState = $this->realState->paginate('10');
+
+        return response()->json($realState, 200);
+    }
+}
